add2ae7f981defec26816f19fb30c709b6efc5c4
Zahed Shareef <realzahed@gmail.com>
2017-09-16 00:51:06 +0530

    fixed an issue in user model

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,10 +1,8 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    password: DataTypes.STRING
-  })
-}
-
+module.exports = (sequelize, DataTypes) =>
+    sequelize.define('User', {
+        email: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        password: DataTypes.STRING
+    })
